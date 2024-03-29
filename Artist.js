41825db0ae61144b6cef5a71815eb507f23099e1
NathanCioppa <nathan.cioppa@gmail.com>
2024-03-29 14:17:19 -0400

    cleaned code

---
+++ Artist.js
@@ -0,0 +1,11 @@
+export class Artist {
+    constructor(name, id, gender, type, tags, debutAlbumYear, area) {
+        this.name = name
+        this.id = id
+        this.gender = gender
+        this.type = type
+        this.tags = tags
+        this.debutAlbumYear = debutAlbumYear
+        this.area = area
+    }
+}
--- index.js
+++ index.js
@@ -1,94 +1,75 @@
-async function searchLastfm(inputElement) {
-    try {
-        const result = await fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${inputElement.value}&api_key=0d233a8d757fa7ab78f3a5605a7567af&format=json`)
-        const response = await result.json()
-        console.log(response)
-        loadSearchResults(response)
-    } 
-    catch (error) {console.log(error)}
-}
+
+import { Artist } from "./Artist.js"
+
+
 
 export async function searchMusicBrainz(query) {
     try {
-        const result = await fetch(`https://musicbrainz.org/ws/2/artist/?query=${query}&fmt=json`)
-        const response = await result.json()
-
-        displayArtistSearchResults(response.artists)
-
-        //const bestMatch = response.artists[0]
-        //let conflicts = [new MusicBrainzConflict(bestMatch)]
-        
-
-        //response.artists.map(artist => {
-        //    if(artist === bestMatch) return
-//
-        //    if(artist.name === bestMatch.name) conflicts.push(new MusicBrainzConflict(artist))
-        //})
-//
-        //if(conflicts.length > 1) conflicts.map(conflict => {
-        //    document.querySelector('#conflicts').innerHTML+=`
-        //    <div class="conflict-item" musicbrainz-id="${conflict.id}" onclick="selectConflict(this.getAttribute('musicbrainz-id'))">
-        //        ${conflict.description}
-        //    </div>
-        //`}) 
-        //else 
-        //getArtistAlbumDebut(response.artists[0].id)
+        const Result = await fetch(`https://musicbrainz.org/ws/2/artist/?query=${query}&fmt=json`)
+        const Response = await Result.json()
+        displayArtistSearchResults(Response.artists)
     } 
     catch (error) {console.log(error)}
 }
 
-function selectConflict(id) {
-    document.querySelector('#conflicts').innerHTML = ''
-    getArtistAlbumDebut(id)
-    
-}
 
+
+// Searches through every release from the given artist on MusicBrainz to determine the earliest released album, their debut album.
+// In some cases, mostly for new artists, no debut album will be returned because they do not have one.
 async function getArtistAlbumDebut(musicBrainzId) {
-    let albumDebutYear = null
+    let albumDebutYear
     let releaseCount = 0
-    const limit = 100
-    let i = 0
+    const Limit = 100
+    let timesSearched = 0
+
     try{
-        do{
-            const result = await fetch(`https://musicbrainz.org/ws/2/release-group/?artist=${musicBrainzId}&limit=${limit}&offset=${limit*i}&fmt=json`)
-            const response = await result.json()
+        do {
+            const Result = await fetch(`https://musicbrainz.org/ws/2/release-group/?artist=${musicBrainzId}&limit=${Limit}&offset=${Limit*timesSearched}&fmt=json`)
+            const Response = await Result.json()
+            
+            // The response will tell the total number of releases, but will not return more than 100.
+            // This can be used to figure out if all releases have been checked, or if more requests are needed. 
+            releaseCount = Response["release-group-count"] 
 
-            releaseCount = response["release-group-count"]
-
-            response["release-groups"].map(release => {
+            // 'release-groups' property contains all releases from the artist
+            Response["release-groups"].map(release => {
                 let isNotAlbum = 
                     release["primary-type"] !== "Album" 
                     || release["secondary-types"].includes("Demo") 
                     || release["secondary-types"].includes("Live")
 
-                if(isNotAlbum)return;
+                if(isNotAlbum) return
                 
-                let releaseYear = null
+                let releaseYear
+                // Sometimes, the release date is in just 'year' or 'year-month-day' format. Only the year is needed.
                 if(release["first-release-date"].length >= 4) releaseYear = release["first-release-date"].substring(0,4)
                 if(releaseYear == null) return
                 
                 releaseYear = Number(releaseYear)
-                if(albumDebutYear > releaseYear || albumDebutYear == null) 
+                if(releaseYear < albumDebutYear || albumDebutYear == null) 
                     albumDebutYear = releaseYear
             })
-            i++
+            timesSearched++
         } 
-        while ((i*limit) < releaseCount)
+        while (timesSearched*Limit < releaseCount)
 
         return albumDebutYear
     }
     catch (error) {console.log(error)}
 }
 
+
+
 let currentlyDisplayedArtists = []
+
 async function displayArtistSearchResults(artists) {
-    const resultsDisplay = document.querySelector("#artist-search-results")
-    resultsDisplay.innerHTML = ""
+    const ResultsDisplay = document.querySelector("#artist-search-results")
+    ResultsDisplay.innerHTML = ""
 
     currentlyDisplayedArtists = artists
     
     artists.map(artist => {
-        resultsDisplay.innerHTML+=`
+        ResultsDisplay.innerHTML+=`
         <div class="artist-search-result" artist-name="${artist.name}" artist-description="${artist.disambiguation}" musicbrainz-id="${artist.id}">
         <span class="artist-name">${artist.name}</span>
         <span class="artist-description">${artist.disambiguation ?? ' - '}</span>
@@ -96,8 +77,10 @@ async function displayArtistSearchResults(artists) {
     `})
 }
 
+
+
 export function selectArtist(artistElement) {
-    let selectedArtist = null
+    let selectedArtist
     currentlyDisplayedArtists.map(displayedArtist => {
         if(displayedArtist.id === artistElement.getAttribute('musicbrainz-id')) {
             selectedArtist = displayedArtist
@@ -108,23 +91,28 @@ export function selectArtist(artistElement) {
     constructArtistProfile(selectedArtist)
 }
 
+
+
+// Gets up to 5 tags for the artist. Tags are basically genres.
 async function getTags(musicBrainzArtist) {
     let tags = musicBrainzArtist.tags
-    let topTags = []
-    console.log(musicBrainzArtist)
+    let topTags = [] // This will contain the final tags that get returned.
 
-    const desiredTagAmount = 5
-    if(tags && tags.length >=desiredTagAmount) {
-        for(let i=0; i<desiredTagAmount; i++) {
+    const DesiredTagAmount = 5
+    if(tags && tags.length >= DesiredTagAmount) {
+        for(let i=0; i<DesiredTagAmount; i++) {
             topTags.push(tags[i].name)
         }
         return topTags
     }
 
+    // If there are not 5 tags listed on the artist's MusicBrainz profile, then tags will be gotten from their Last.fm profile.
+    // Last.fm never gives more than 5 tags, and rarely gives less than 5.
     try{
         let result = await fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&mbid=${musicBrainzArtist.id}&api_key=0d233a8d757fa7ab78f3a5605a7567af&format=json`)
         let response = await result.json()
         
+        // If the is no artist listed on last.fm under the specified MusicBrainz id, a search by name will be preformed instead. 
         if(response.error) {
             result = await fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${musicBrainzArtist.name}&api_key=0d233a8d757fa7ab78f3a5605a7567af&format=json`)
             response = await result.json()
@@ -139,14 +127,19 @@ async function getTags(musicBrainzArtist) {
     return topTags
 }
 
+
+
+// Recursively searches through related areas until the country is found in cases where the area is not a country .
 async function getCountry(musicBrainzArea) {
     if(musicBrainzArea.type === "Country") return musicBrainzArea.name
     let searchId
 
-    if(musicBrainzArea.relations){
+    if(musicBrainzArea.relations) {
         let foundCountry = false
         let countryName
         musicBrainzArea.relations.map(relatedArea => {
+            // "backward" direction indicates that the related area contains the current area.
+            // Ex. if current area is California, the related "backward" area would be United States.
             if(relatedArea.direction === "backward") {
                 searchId = relatedArea.area.id
                 countryName = relatedArea.area.name
@@ -160,9 +153,9 @@ async function getCountry(musicBrainzArea) {
     else {searchId = musicBrainzArea.id}
 
     try{
-        const result = await fetch(`https://musicbrainz.org/ws/2/area/${searchId}?inc=area-rels&fmt=json`)
-        const response = await result.json()
-        return await getCountry(response)
+        const Result = await fetch(`https://musicbrainz.org/ws/2/area/${searchId}?inc=area-rels&fmt=json`)
+        const Response = await Result.json()
+        return await getCountry(Response)
     }
     catch(error) {console.log(error)}
     
@@ -170,8 +163,9 @@ async function getCountry(musicBrainzArea) {
 }
 
 
-async function constructArtistProfile(selectedArtist) {
 
+// Creates the Artist object that will be used in the game.
+async function constructArtistProfile(selectedArtist) {
     let artistType
     if(selectedArtist.type === "Person") artistType = "Individual"
     if(selectedArtist.type === "Group") artistType = "Group"
@@ -183,38 +177,31 @@ async function constructArtistProfile(selectedArtist) {
         artistType,
         await getTags(selectedArtist),
         await getArtistAlbumDebut(selectedArtist.id),
-        selectedArtist.area.type === "Country" ? selectedArtist.area.name : await getCountry(selectedArtist.area)
+        selectedArtist.area ? await getCountry(selectedArtist.area) : null
     )
 
     console.log(artist)
 }
 
-class Artist {
-    constructor(name, id, gender, type, tags, debutAlbumYear, area) {
-        this.name = name
-        this.id = id
-        this.gender = gender
-        this.type = type
-        this.tags = tags
-        this.debutAlbumYear = debutAlbumYear
-        this.area = area
-    }
-}
 
-class MusicBrainzConflict {
-    constructor(musicBrainzArtistJson) {
-        this.description = musicBrainzArtistJson.disambiguation
-        this.id = musicBrainzArtistJson.id
-    }
-}
 
 /*
 Get an artist's tags and onTour
 let ARTIST_NAME = LAST_FM_SEARCH_RESULTS_OBJECT.results.artistmatches.artist[0].name
 try {
-        const result = await fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${ARTIST_NAME}&api_key=0d233a8d757fa7ab78f3a5605a7567af&format=json`)
-        const response = await result.json()
-        console.log(response)
+    const result = await fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${ARTIST_NAME}&api_key=0d233a8d757fa7ab78f3a5605a7567af&format=json`)
+    const response = await result.json()
+    console.log(response)
+} 
+    catch (error) {console.log(error)}
+
+async function searchLastfm(inputElement) {
+    try {
+        const Result = await fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${inputElement.value}&api_key=0d233a8d757fa7ab78f3a5605a7567af&format=json`)
+        const Response = await Result.json()
+        console.log(Response)
+        loadSearchResults(Response)
     } 
     catch (error) {console.log(error)}
+}
 */
